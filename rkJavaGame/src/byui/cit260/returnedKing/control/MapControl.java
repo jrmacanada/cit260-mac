16901205bec9e6e34d26c262d3f8c0a31f10ce28
michaelcavey <michaelcavey@michaels-imac>
2016-07-07 08:40:47 -0700

    7/7/2016 - Added scene descriptions to 18 scenes in array.

--- rkJavaGame/src/byui/cit260/returnedKing/control/MapControl.java
+++ rkJavaGame/src/byui/cit260/returnedKing/control/MapControl.java
@@ -76,42 +76,63 @@ public class MapControl {
         Scene[] scenes = new Scene[SceneType.values().length];
 
         Scene monasteryScene = new Scene();
-        monasteryScene.setDescription("ADD DESCRIPTION OF monastery LATER");
+        monasteryScene.setDescription(" Monastery: "
+                + "\n This scene is the starting point for a Monk."
+                + "\n You'll be lucky if you ever return.");
         monasteryScene.setMapSymbol("MM");
         scenes[SceneType.monastery.ordinal()] = monasteryScene;
 
         Scene graveyardScene = new Scene();
-        graveyardScene.setDescription("ADD DESCRIPTION OF graveyard LATER");
+        graveyardScene.setDescription(" Graveyard:"
+                + "\n Rest one day to gain 15 Stamina points."
+                + "\n See if you can find some useful items."
+                + "\n Read the gravestones to learn more.");
         graveyardScene.setMapSymbol("GY");
         scenes[SceneType.graveyard.ordinal()] = graveyardScene;
 
         Scene northRoadScene = new Scene();
-        northRoadScene.setDescription("ADD DESCRIPTION OF northRoad LATER");
+        northRoadScene.setDescription(" North Road:"
+                + "\n Prison guard blocks forward movement."
+                + "\n You cannot move forward in your present direction."
+                + "\n You will be rerouted if you choose not to fight.");
         northRoadScene.setMapSymbol("NR");
         scenes[SceneType.northRoad.ordinal()] = northRoadScene;
 
         Scene prisonScene = new Scene();
-        prisonScene.setDescription("ADD DESCRIPTION OF prison LATER");
+        prisonScene.setDescription(" Prison:"
+                + "\n Learn what you can inside and outside the prison."
+                + "\n Earn 20 coins for working a job as a guard.");
         prisonScene.setMapSymbol("PP");
         scenes[SceneType.prison.ordinal()] = prisonScene;
 
         Scene warriorVillageScene = new Scene();
-        warriorVillageScene.setDescription("ADD DESCRIPTION OF warriorVillage LATER");
+        warriorVillageScene.setDescription(" Warrior Village:"
+                + "\n This scene is the starting point for a Warrior."
+                + "\n You'll be lucky if you ever return.");
         warriorVillageScene.setMapSymbol("WV");
         scenes[SceneType.warriorVillage.ordinal()] = warriorVillageScene;
 
         Scene cornfieldScene = new Scene();
-        cornfieldScene.setDescription("ADD DESCRIPTION OF cornfield LATER");
+        cornfieldScene.setDescription(" Cornfield:"
+                + "\n Rest one day to gain 15 Stamina points."
+                + "\n Glean from the fields to replenish food."
+                + "\n Work with the farmer to earn 10 coins.");
         cornfieldScene.setMapSymbol("CF");
         scenes[SceneType.cornfield.ordinal()] = cornfieldScene;
 
         Scene farmersVillageScene = new Scene();
-        farmersVillageScene.setDescription("ADD DESCRIPTION OF farmersVillage LATER");
+        farmersVillageScene.setDescription(" Farming Village:"
+                + "\n The farm village is a great place to replenish your supplies."
+                + "\n Ask around for a job to earn more coin."
+                + "\n The villagers are friendly, but skeptical of strangers.");
         farmersVillageScene.setMapSymbol("FV");
         scenes[SceneType.farmersVillage.ordinal()] = farmersVillageScene;
 
         Scene northGateScene = new Scene();
-        northGateScene.setDescription("ADD DESCRIPTION OF northGate LATER");
+        northGateScene.setDescription(" North Gate to Castle:"
+                + "\n The north gate is the main enterance to the castle."
+                + "\n Twenty guards guard the enterance with hundreds more inside."
+                + "\n Villagers may pass only if they give up their arms.");
         northGateScene.setMapSymbol("NG");
         scenes[SceneType.northGate.ordinal()] = northGateScene;
 
@@ -126,12 +147,19 @@ public class MapControl {
         scenes[SceneType.tavern.ordinal()] = tavernScene;
 
         Scene westRoadScene = new Scene();
-        westRoadScene.setDescription("ADD DESCRIPTION OF westRoad LATER");
+        westRoadScene.setDescription(" West Road:"
+                + "\n Rogue warrior blocks forward movement."
+                + "\n You cannot move forward in your present direction."
+                + "\n You will be rerouted if you choose not to fight.");
         westRoadScene.setMapSymbol("WR");
         scenes[SceneType.westRoad.ordinal()] = westRoadScene;
 
         Scene westGateScene = new Scene();
-        westGateScene.setDescription("ADD DESCRIPTION OF westGate LATER");
+        westGateScene.setDescription(" West Gate to Castle:"
+                + "\n The West gate is always locked."
+                + "\n No guards are stationed at the enterance because a swamp "
+                + "\n filled with crocodiles and snakes prevent access."
+                + "\n Nobody has ever fought their way through.");
         westGateScene.setMapSymbol("WG");
         scenes[SceneType.westGate.ordinal()] = westGateScene;
 
@@ -141,12 +169,18 @@ public class MapControl {
         scenes[SceneType.castle.ordinal()] = castleScene;
 
         Scene eastGateScene = new Scene();
-        eastGateScene.setDescription("ADD DESCRIPTION OF eastGate LATER");
+        eastGateScene.setDescription(" East Gate to Castle:"
+                + "\n The east gate is monatored by a corrupt guard."
+                + "\n For 100 coins the guard will allow you to pass, no questions asked."
+                + "\n Postitutes from town and the tavern are allowed access.");
         eastGateScene.setMapSymbol("EG");
         scenes[SceneType.eastGate.ordinal()] = eastGateScene;
 
         Scene eastRoadScene = new Scene();
-        eastRoadScene.setDescription("ADD DESCRIPTION OF eastRoad LATER");
+        eastRoadScene.setDescription(" East Road:"
+                + "\n Bandit blocks forward movement."
+                + "\n You cannot move forward in your present direction."
+                + "\n You will be rerouted if you choose not to fight.");
         eastRoadScene.setMapSymbol("ER");
         scenes[SceneType.eastRoad.ordinal()] = eastRoadScene;
 
@@ -161,12 +195,18 @@ public class MapControl {
         scenes[SceneType.huntingReserve.ordinal()] = huntingReserveScene;
 
         Scene southGateScene = new Scene();
-        southGateScene.setDescription("ADD DESCRIPTION OF southGate LATER");
+        southGateScene.setDescription(" South Gate to Castle:"
+                + "\n The south gate is always locked."
+                + "\n One guard protects the enterance."
+                + "\n A password is required to be allowed into the castle.");
         southGateScene.setMapSymbol("SG");
         scenes[SceneType.southGate.ordinal()] = southGateScene;
 
         Scene townshipScene = new Scene();
-        townshipScene.setDescription("ADD DESCRIPTION OF township LATER");
+        townshipScene.setDescription(" Township:"
+                + "\n The township brings back many fond childhood memories."
+                + "\n Your father would walk the streets with you without fear."
+                + "\n This is the place you escaped to when fleeing your murderous uncle.");
         townshipScene.setMapSymbol("TS");
         scenes[SceneType.township.ordinal()] = townshipScene;
 
@@ -176,17 +216,25 @@ public class MapControl {
         scenes[SceneType.church.ordinal()] = churchScene;
 
         Scene wizardsHamletScene = new Scene();
-        wizardsHamletScene.setDescription("ADD DESCRIPTION OF wizardsHamlet LATER");
+        wizardsHamletScene.setDescription(" Wizard Hamlet:"
+                + "\n This scene is the starting point for the Wizard."
+                + "\n You'll be lucky if you ever return.");
         wizardsHamletScene.setMapSymbol("WH");
         scenes[SceneType.wizardsHamlet.ordinal()] = wizardsHamletScene;
 
         Scene secretCaveScene = new Scene();
-        secretCaveScene.setDescription("ADD DESCRIPTION OF secretCave LATER");
+        secretCaveScene.setDescription(" Secret Cave:"
+                + "\n Rest one day to gain 20 Stamina points."
+                + "\n See if you can find some useful items."
+                + "\n The skeletons should tell you something.");
         secretCaveScene.setMapSymbol("SC");
         scenes[SceneType.secretCave.ordinal()] = secretCaveScene;
 
         Scene southRoadScene = new Scene();
-        southRoadScene.setDescription("ADD DESCRIPTION OF southRoad LATER");
+        southRoadScene.setDescription(" South Road:"
+                + "\n Drucken sailor blocks forward movement."
+                + "\n You cannot move forward in your present direction."
+                + "\n You will be rerouted if you choose not to fight.");
         southRoadScene.setMapSymbol("SR");
         scenes[SceneType.southRoad.ordinal()] = southRoadScene;
 
@@ -196,7 +244,10 @@ public class MapControl {
         scenes[SceneType.harborTown.ordinal()] = harborTownScene;
 
         Scene seaportScene = new Scene();
-        seaportScene.setDescription("ADD DESCRIPTION OF seaport LATER");
+        seaportScene.setDescription(" Seaport:"
+                + "\n The seaport is a dangerous place that plays by its own rules."
+                + "\n Foriegn travelers have never been subject to the King's rule."
+                + "\n Neither have they been rebelous. They only defend their iberty");
         seaportScene.setMapSymbol("SP");
         scenes[SceneType.seaport.ordinal()] = seaportScene;
 
